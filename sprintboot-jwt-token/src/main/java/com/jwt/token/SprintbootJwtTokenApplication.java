dfea4c751f76e32fc6ff00ab63fecb9266b5631d
manikarthi25 <manikarthi25@gmail.com>
2022-05-10 19:42:56 +0530

    Initial Commit

---
+++ sprintboot-jwt-token/src/main/java/com/jwt/token/SprintbootJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootJwtTokenApplication.class, args);
+	}
+
+}
